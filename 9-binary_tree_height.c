4749c52bffa87b8ce540c05b633f69fee2079501
farsightedEnvy <FarsightedEnvy>
2024-07-03 21:46:50 -0700

    bug fixes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,19 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - print elements of tree using post-order traversal
+ * binary_tree_height - Function that measures the height of a binary tree
  * @tree: tree to go through
- * @func: function to use
- * Return: Nothing
+ * Return: the height
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
-{
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
 
-    binary_tree_postorder(tree->left, func);
-    binary_tree_postorder(tree->right, func);
-    func(tree->n);
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l = 0;
+	size_t r = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
